3456f9cac322842388022d361a637be31be53b33
Lin-Xing Zeng <jasonphysics@outlook.com>
2023-11-07 17:43:31 +0800

    Now I have learned to disable the displaying of input, and switch to a new way to process the so called 'any key' to continue. Therefore it would cost less time.

--- pause.c
+++ pause.c
@@ -5,10 +5,25 @@ Email:  jasonphysics@outlook.com | jasonphysics19@gmail.com
 
 This file is part of Linux-pause, in ./pause.c
 */
-#include<stdlib.h>
+#include<stdio.h>
+#include<termios.h>
+#include<unistd.h>
+
+#define N 8
 
 int main(int argc,char *argv[])
 {
-    system("bash -c \"read -n 1 -s -r -p 'Press any key to continue'\"; echo");
+    /* change the setting of the terminal, without showing the input */
+    struct termios old_term, new_term;
+	tcgetattr(STDIN_FILENO, &old_term);
+	new_term = old_term;
+	new_term.c_lflag &= ~(ECHO | ICANON); // disable displaying the input and line buffer
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+	/* display */
+	printf("Press any key to continue");
+    volatile int c = getchar();
+	printf("\n");
+	/* restore the terminal's setting */
+	tcsetattr(STDIN_FILENO, TCSANOW,&old_term);
     return 0;
 }
