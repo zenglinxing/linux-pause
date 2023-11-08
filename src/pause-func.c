8db30ad5fb7b4f8aee790d36486ca72b820e12be
Lin-Xing Zeng <jasonphysics@outlook.com>
2023-11-08 10:33:07 +0800

    Update support for Windows.

--- src/pause-func.c
+++ src/pause-func.c
@@ -6,11 +6,20 @@ Email:  jasonphysics@outlook.com | jasonphysics19@gmail.com
 This file is part of Linux-pause, in ./src/pause-func.c
 */
 #include<stdio.h>
+#ifdef _WIN32
+#include<conio.h>
+#else
 #include<termios.h>
 #include<unistd.h>
+#endif
 
 int winpause(void)
 {
+#ifdef _WIN32
+	printf("Press any key to continue");
+    volatile int c = _getch();
+	printf("\n");
+#else
     /* change the setting of the terminal, without showing the input */
     struct termios old_terminal, new_terminal;
 	tcgetattr(STDIN_FILENO, &old_terminal);
@@ -23,5 +32,6 @@ int winpause(void)
 	printf("\n");
 	/* restore the terminal's setting */
 	tcsetattr(STDIN_FILENO, TCSANOW,&old_terminal);
+#endif /* _WIN32 */
     return c;
 }
--- src/pause.c
+++ src/pause.c
@@ -5,13 +5,8 @@ Email:  jasonphysics@outlook.com | jasonphysics19@gmail.com
 
 This file is part of Linux-pause, in ./src/pause.c
 */
-#include<stdio.h>
-#include<termios.h>
-#include<unistd.h>
 
 #include"pause-func.h"
-
-#define N 8
 
 int main(int argc,char *argv[])
 {
